69df8e83f06dd8b9f0cefde6895197e57077c0d3
zhengqiu.jia <zheng qiu.jia@xiaoyangedu.com>
2023-10-17 16:27:22 +0800

    b

---
+++ scripts/verifyCommit.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+// const childProcess = require('child_process')
+const chalk = require('chalk')
+const fs = require('fs')
+// const email = childProcess
+//   .execSync('git config user.email')
+//   .toString()
+//   .trim()
+const msg = fs.readFileSync(process.argv[2], 'utf-8').trim() // 索引 2 对应的 commit 消息文件
+const commitRE = /^(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|release|workflow)(\(.+\))?: .{1,100}/
+if (!commitRE.test(msg)) {
+  /* eslint-disable */
+  console.error(
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
+      'invalid commit message format.'
+    )}\n\n` +
+      chalk.red(
+        '  Proper commit message format is required for automated changelog generation. Examples:\n\n'
+      ) +
+      `    ${chalk.green('feat(compiler): add "comments" option')}\n` +
+      `    ${chalk.green(
+        'fix(v-model): handle events on blur (close #28)'
+      )}\n\n` +
+      chalk.red(
+        '详情请查看 git commit 提交规范：https://github.com/woai3c/Front-end-articles/blob/master/git%20commit%20style.md'
+      )
+  )
+  process.exit(1)
+}
+
+// if (!/@(xiaoyangedu\.com|xiaoyanggroup\.cn)$/.test(email)) {
+//   console.error(
+//     '此用户没有权限，具有权限的用户为： xxx@xiaoyangedu.com 或 xxx@xiaoyanggroup.cn'
+//   )
+//   process.exit(1)
+// }
